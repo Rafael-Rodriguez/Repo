6b8467df4422c9663da49658aec95a1a8da2e9d4
Rafael-Rodriguez <rrodrigu@gmail.com>
2018-07-15 21:01:07 -0700

    Chapter19.CustomerInvoices:  Initial checkin.  Simple application that grabs Customer information from both the Customer and Invoices table.

---
+++ Chapter19.CustomerInvoices/Chapter19.CustomerInvoices/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Chapter19.CustomerInvoices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void fillByCustomerIDToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var customerID = Convert.ToInt32(customerIDToolStripTextBox.Text);
+                this.customersTableAdapter.FillByCustomerID(this.mMABooksDataSet.Customers, customerID);
+                this.invoicesTableAdapter.FillByCustomerID(this.mMABooksDataSet.Invoices, customerID);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        /*private void fillByCustomerIDToolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.invoicesTableAdapter.FillByCustomerID(this.mMABooksDataSet.Invoices, ((int)(System.Convert.ChangeType(customerIDToolStripTextBox1.Text, typeof(int)))));
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }*/
+    }
+}
