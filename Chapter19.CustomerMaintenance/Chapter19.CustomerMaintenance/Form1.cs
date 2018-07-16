a9be2452bf2a37c049ae268d2c6a01def0782512
Rafael-Rodriguez <rrodrigu@gmail.com>
2018-07-15 17:29:11 -0700

    Chapter19.CustomerMaintenance: Simple application that uses two db tables (Customer, States) to fill in individual customer information.  Additional functionality includes a GetByCustomerID and GetAllCustomers buttons.

---
+++ Chapter19.CustomerMaintenance/Chapter19.CustomerMaintenance/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Chapter19.CustomerMaintenance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.mMABooksDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'mMABooksDataSet.States' table. You can move, or remove it, as needed.
+            this.statesTableAdapter.Fill(this.mMABooksDataSet.States);
+        }
+
+        private void fillByCustomerIDToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var customerID = Convert.ToInt32(customerIDToolStripTextBox.Text);
+                this.customersTableAdapter.FillByCustomerID(this.mMABooksDataSet.Customers, customerID);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void fillToolStripButton_Click(object sender, EventArgs e)
+        {
+            customersTableAdapter.Fill(mMABooksDataSet.Customers);
+        }
+    }
+}
