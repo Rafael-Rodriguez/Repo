e08dbc4554acd1a974a3109ad51b8688ec65aef9
Rafael-Rodriguez <rrodrigu@gmail.com>
2018-09-07 10:50:30 -0700

    Added a new controller for frmCustomerDisplay.

---
+++ Chapter24.CustomerMaintenance/Chapter24.CustomerMaintenance/Controllers/frmCustomerDisplayController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using Chapter24.CustomerMaintenance.Model;
+using Chapter24.CustomerMaintenance.Perspectives;
+
+namespace Chapter24.CustomerMaintenance.Controllers
+{
+    public class frmCustomerDisplayController
+    {
+        private MMABooksEntities _dbContext;
+        private object _syncLock = new object();
+
+        public frmCustomerDisplayController(frmCustomerDisplay parentForm)
+        {
+            View = parentForm;
+        }
+
+        private frmCustomerDisplay View { get; set; }
+
+        private MMABooksEntities DbContext
+        {
+            get
+            {
+                if(_dbContext == null)
+                {
+                    lock(_syncLock)
+                    {
+                        _dbContext = new MMABooksEntities();
+                    }
+                }
+
+                return _dbContext;
+            }
+        }
+
+        internal void GetCustomer(string customerIdText)
+        {
+            int customerID = Convert.ToInt32(customerIdText);
+            GetCustomer(customerID);
+        }
+
+        private void GetCustomer(int customerID)
+        {
+            try
+            {
+                var matchingCustomer = DbContext.Customers.Where(customer => customer.CustomerID == customerID).SingleOrDefault();
+
+                if (matchingCustomer == null)
+                {
+                    MessageBox.Show("No customer found with this ID." + "Please try again.", "Customer Not Found");
+                    View.ClearControls();
+                }
+                else
+                {
+                    if (!DbContext.Entry(matchingCustomer).Reference("State").IsLoaded)
+                    {
+                        DbContext.Entry(matchingCustomer).Reference("State").Load();
+                    }
+
+                    View.DisplayCustomer(matchingCustomer);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+
+        }
+    }
+}
--- Chapter24.CustomerMaintenance/Chapter24.CustomerMaintenance/Perspectives/frmCustomerDisplay.cs
+++ Chapter24.CustomerMaintenance/Chapter24.CustomerMaintenance/Perspectives/frmCustomerDisplay.cs
@@ -1,72 +1,58 @@
-﻿using Chapter24.CustomerMaintenance.Model;
+﻿using Chapter24.CustomerMaintenance.Controllers;
+using Chapter24.CustomerMaintenance.Model;
 using Chapter24.CustomerMaintenance.Perspectives.Extensions;
 using System;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Chapter24.CustomerMaintenance.Perspectives
 {
     public partial class frmCustomerDisplay : Form
     {
-        private Customer selectedCustomer;
+        private frmCustomerDisplayController _controller;
+        private object _syncLock = new object();
 
         public frmCustomerDisplay()
         {
             InitializeComponent();
         }
 
+        private frmCustomerDisplayController Controller
+        {
+            get
+            {
+                if(_controller == null)
+                {
+                    lock(_syncLock)
+                    {
+                        _controller = new frmCustomerDisplayController(this);
+                    }
+                }
+
+                return _controller;
+            }
+        }
+
         private void btnGetCustomer_Click(object sender, EventArgs e)
         {
             if(txtBoxCustomerID.IsPresent() && txtBoxCustomerID.IsInt32())
             {
-                int customerID = Convert.ToInt32(txtBoxCustomerID.Text);
-                GetCustomer(customerID);
+                Controller.GetCustomer(txtBoxCustomerID.Text);
             }
         }
 
-        private void GetCustomer(int customerID)
+        internal void DisplayCustomer(Customer customer)
         {
-            try
-            {
-                selectedCustomer = MMABooksEntity.mmaBooks.Customers.Where(customer => customer.CustomerID == customerID).Select(customer => customer).SingleOrDefault();
-
-                if(selectedCustomer == null)
-                {
-                    MessageBox.Show("No customer found with this ID." + "Please try again.", "Customer Not Found");
-                    ClearControls();
-                    txtBoxCustomerID.Focus();
-                }
-                else
-                {
-                    if(!MMABooksEntity.mmaBooks.Entry(selectedCustomer).Reference("State").IsLoaded)
-                    {
-                        MMABooksEntity.mmaBooks.Entry(selectedCustomer).Reference("State").Load();
-                    }
-
-                    DisplayCustomer();
-                }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message, ex.GetType().ToString());
-            }
-
-        }
-
-        private void DisplayCustomer()
-        {
-            txtBoxName.Text = selectedCustomer.Name;
-            txtBoxAddress.Text = selectedCustomer.Address;
-            txtBoxCity.Text = selectedCustomer.City;
-            txtBoxState.Text = selectedCustomer.State.StateName;
-            txtBoxZipCode.Text = selectedCustomer.ZipCode;
+            txtBoxName.Text = customer.Name;
+            txtBoxAddress.Text = customer.Address;
+            txtBoxCity.Text = customer.City;
+            txtBoxState.Text = customer.State.StateName;
+            txtBoxZipCode.Text = customer.ZipCode;
 
             btnModifyCustomer.Enabled = true;
             btnDeleteCustomer.Enabled = true;
-
         }
 
-        private void ClearControls()
+        internal void ClearControls()
         {
             txtBoxName.Text = "";
             txtBoxAddress.Text = "";
@@ -74,6 +60,9 @@ namespace Chapter24.CustomerMaintenance.Perspectives
             txtBoxCustomerID.Text = "";
             txtBoxState.Text = "";
             txtBoxZipCode.Text = "";
+
+            txtBoxCustomerID.Focus();
+
             btnModifyCustomer.Enabled = false;
             btnDeleteCustomer.Enabled = false;
         }
