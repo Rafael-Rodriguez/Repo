00c72a587431f14ec3cdfbab9e5bbe9b20db6951
Rafael-Rodriguez <rrodrigu@gmail.com>
2019-09-25 21:59:43 -0700

    Initial commit.  Non-functional

---
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            return 0;
+        }
+
+        public string LongestSubstring(string s)
+        {
+            return s;
+        }
+    }
+}
